79b5508bb36ee7f112b1b26221551acb48a05600
leandrorossa87 <leandrorossa87@gmail.com>
2022-08-19 02:46:37 -0300

    optimizacion de rutas views

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const fs = require('fs')
 
 const express = require('express')
 const app = express()
@@ -16,11 +17,55 @@ app.get('/productos', function (req, res) {
   })*/
 
   /* rutas dinamicas*/
-  app.get('/productos/:id', function (req, res) {
+  /*app.get('/productos/:id', function (req, res) {
    
     const id = req.params.id;
     
     res.send(`producto #${id}`)
+  })*/
+
+  /*Ruta dinmica para cargar vista desded html*/ 
+    /*app.get('/productos/:id', function (req, res) {
+   
+    const id = req.params.id;
+
+    if(fs.existsSync(path.join(__dirname, `views/producto${id}.html`))){
+
+      res.sendFile(path.join(__dirname, `views/producto${id}.html`));
+    }
+      res.sendFile(path.join(__dirname, `views/error.html`));
+    
+    
+  })*/
+/*mejoramiento del codigo*/
+  /*app.get('/productos/:id', function (req, res) {
+   
+    const id = req.params.id;
+
+    const ruta = path.join(__dirname, `views/producto${id}.html`)
+
+    if(fs.existsSync(ruta)){
+
+      res.sendFile(ruta);
+    }
+      res.sendFile(path.join(__dirname, `views/error.html`));
+    
+    
+  })*/
+
+  /*optimizacion con Ternarios*/ 
+  app.get('/productos/:id', function (req, res) {
+   
+    const id = req.params.id;
+
+    const filePath = path.join(__dirname, `views/producto${id}.html`)
+    
+    const ruta = fs.existsSync(filePath)
+      ? filePath
+      :path.join(__dirname, `views/error.html`)
+
+      res.sendFile(ruta)
+    
   })
 
 app.listen(3000)
